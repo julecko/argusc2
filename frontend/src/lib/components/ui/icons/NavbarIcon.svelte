1628d817e8a044c9b6b0bb5aef42602011ad2667
Julecko <julecko@dilino.sk>
2026-03-17 14:09:53 +0100

    Added universal icon component

---
+++ frontend/src/lib/components/ui/icons/NavbarIcon.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import UnknownIcon from "$assets/FilenotfoundIcon.svg?raw"
+    export let size: number = 24;
+	export let color: string = 'currentColor';
+	export let svg: string = '';
+</script>
+
+{#if svg}
+	<div
+		class="nav-icon"
+		style="width: {size}px; height: {size}px; color: {color};"
+	>
+		{@html svg}
+	</div>
+{:else}
+	<div
+		class="nav-icon"
+		style="width: {size}px; height: {size}px; color: {color};"
+	>
+        {@html UnknownIcon}
+    </div>
+{/if}
+
+<style>
+	.nav-icon {
+        display: inline-block;
+    }
+	.nav-icon svg {
+		width: 100%;
+		height: 100%;
+		stroke: currentColor;
+		fill: none;
+		stroke-width: 2;
+		stroke-linecap: round;
+		stroke-linejoin: round;
+	}
+</style>
